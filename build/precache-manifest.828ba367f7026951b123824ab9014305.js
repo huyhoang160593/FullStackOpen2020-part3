051ea6ff2a9d37f231460c80415fe16f66d6a6c1
iulover99 <iulover99@gmail.com>
2020-06-18 14:35:41 +0700

    uibuild

---
+++ build/precache-manifest.828ba367f7026951b123824ab9014305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7cf5afdf923fa70d0bf6562c5dd2aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efddfe295cec71a558e3",
+    "url": "/static/css/main.2cd743b4.chunk.css"
+  },
+  {
+    "revision": "c2db7b13b6c244651373",
+    "url": "/static/js/2.d5e710d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5e710d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efddfe295cec71a558e3",
+    "url": "/static/js/main.0099185c.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
